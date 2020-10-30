47a7644bc74e1e6465f4a12cfea2592b3962c6e6
unknown <acoidanfcb@gmail.com>
2020-10-30 19:55:15 +0000

    acoidanbike

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Message from './components/Message';
+import AcoidanBike from './components/AcoidanBike';
 
 function App() {
   return (
     <div>
       <p>Hola soy un desarrollador muy optimista</p>
       <Message />
+      <AcoidanBike />
     </div>
   );
 }
